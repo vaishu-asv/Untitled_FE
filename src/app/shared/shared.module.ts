0aaea6fbb9596fd88d403ca5e3300b06a7e7bbda
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-20 14:48:52 +0530

    Initial commit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,17 +4,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   providers: [
     AuthenticationGuard
   ],
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     SharedRoutingModule
   ,	QuickUiModule,
-]
+],
+  exports: [
+    HeaderComponent
+  ],
 })
 export class SharedModule { }
